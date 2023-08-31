501d0653979da91cec8b27d82b3a121d39071cad
lahouely <lahouel.youssouf@gmail.com>
2023-08-31 19:20:15 +0200

    add conditions

---
+++ conditions.js
@@ -0,0 +1,29 @@
+if(true){
+    ///
+}
+
+if(true){
+    ///
+}else{
+    ///
+}
+
+if(true){
+    ///
+}else if(false){
+    ///
+}else{
+    ///
+}
+
+switch (randomColor){
+    case 'orange':
+        console.log('the color is orange');
+        break;
+    
+    case 'blue':
+        console.log('the color is blue');
+        break;
+    default:
+        console.log('not found');    
+}
