9aeaf2326d792bf82434ded7d809b68732d0e6bd
lahouely <lahouel.youssouf@gmail.com>
2023-08-31 17:52:20 +0200

    Arrays

---
+++ arrays.js
@@ -0,0 +1,7 @@
+const myArray = [10, 30, 60];
+const myOtherArray = [10, 'my string', {age: 36, color: 'blue'}, [1, 2] ];
+console.log(myArray[1]); //30
+console.log(myOtherArray[3][1]); //2
+console.log(myOtherArray[2].color); //'blue'
+console.log(myArray)
+console.log(myArray.reverse())
