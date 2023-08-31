29c15f483c958ffb5a0a5f8fdd00638777c5fba3
lahouely <lahouel.youssouf@gmail.com>
2023-08-31 17:40:29 +0200

    variables and datatypes

---
+++ variables.js
@@ -0,0 +1,17 @@
+const var1 = 67;
+console.log(var1);
+let var2;
+var2 = 5;
+var2 = var2 + 2;
+var2 ++;
+var var3;           //legacy, bad, don't use
+var3 = "there";
+const var4 = false
+typeof var1;//number
+typeof var3;//string
+typeof var4;//boolean
+
+const v1 = "13";
+const v2 = 11;
+console.log(v1+v2);
+console.log(Number(v1)+v2);
