053d63b51f51c65a97815bc95cda825805229fd1
lahouely <lahouel.youssouf@gmail.com>
2023-08-31 18:49:25 +0200

    operators

---
+++ operators.js
@@ -0,0 +1,40 @@
+//arithmetic operators
+
+20+50;
+50-20;
+20/2;
+2*2;
+7%2;
+2**10;//1024
+let v=5;
+v++;
+v--;
+v+=3;
+v-=9;
+v*=3;
+v/=2;
+
+//comparison operators
+
+20>10
+20<10
+20>=10
+20<=10
+20 ===  20  //true: same type and value
+20 === '20' //false: same value but diffrent types
+20 ==  '20'  //true: same value, type doesn't matter
+20 !== 20   //negation of ===
+20 != '20'   //negation of ==
+
+// arrays and objects can't be compared using these operators
+
+//ternary 
+200 >= 20 ? 200 : 20 ;
+
+//logical operators
+2>3 && 5<5
+2>3 || 5<5
+!(3<6)
+ 
+
+ 
