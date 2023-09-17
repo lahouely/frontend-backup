42393721507d922b09664f779ae7173b54b38877
lahouely <lahouel.youssouf@gmail.com>
2023-09-17 05:08:15 +0200

    Renamed date to ydate

--- nextjs-using-npx/nextjs-blog/components/ydate.js
+++ nextjs-using-npx/nextjs-blog/components/ydate.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns';
 
-export default function Date({ dateString }) {
+export default function YDate({ dateString }) {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'EEEE, \'the\' do \'of\' LLLL, yyyy \'years\' G')}</time>;
 }
